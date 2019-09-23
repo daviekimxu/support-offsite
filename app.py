586d7571aa07ba876e003dcb87d3fbcfaca93ace
David Xu <daviekimxu@live.com>
2019-09-23 15:56:35 -0700

    tetris

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 from scripts.pulse import pulse
 from scripts.rainbow import rainbow
+from scripts.1d_tetris import 1d_tetris
 # from scripts.weather import weather
 # from scripts.twitter import twitter
 import blinkt
 
 def lights():
-    effect = rainbow
+    effect = 1d_tetris
     effect()
 
 if __name__ == '__main__':
